159cdb6f4e64e4cc4ad7cbb998cd71b4033fc7b2
Yağızcan Şahin <85365132+Yagizcan00@users.noreply.github.com>
2024-09-19 19:59:28 +0300

    “Update”

--- app/src/page/Home.jsx
+++ app/src/page/Home.jsx
@@ -2,9 +2,7 @@ import React, { useContext, useState } from 'react'
 import "./Home.scss"
 import MyContext from '../context/MyContext'
 
-
 function Home() {
-
   // Fetch context's data.
   const {
     teamOneCategory,
@@ -23,25 +21,19 @@ function Home() {
     teamTwo
   } = useContext(MyContext);
 
-
   // States for dropdown items.
   const [teamOneCategoryCheck, setTeamOneCategoryCheck] = useState(false)
   const [teamTwoCategoryCheck, setTeamTwoCategoryCheck] = useState(false)
   const [teamOneClassCheck, setTeamOneClassCheck] = useState(false)
   const [teamTwoClassCheck, setTeamTwoClassCheck] = useState(false)
 
-
   return (
     <div className='Home'>
 
       <section className='FirstLine'>
-
         <section className='TeamCategory'>
-
           <button className='TeamCategoryButton' onClick={() => setTeamOneCategoryCheck(!teamOneCategoryCheck)}>Team One Category</button>
-
           <section className={teamOneCategoryCheck ? 'DropdownCategoryActive' : 'DropdownCategoryInactive'}>
-
             {
               categories.map((item) => (
                 <p key={item} onClick={() => setTeamOneCategory(item) & setTeamOneCategoryCheck(!teamOneCategoryCheck)}>
@@ -49,17 +41,11 @@ function Home() {
                 </p>
               ))
             }
-
           </section>
-
         </section>
-
         <section className='TeamCategory'>
-
           <button className='TeamCategoryButton' onClick={() => setTeamTwoCategoryCheck(!teamTwoCategoryCheck)}>Team Two Category</button>
-
           <section className={teamTwoCategoryCheck ? 'DropdownCategoryActive' : 'DropdownCategoryInactive'}>
-
             {
               categories.map((item) => (
                 <p key={item} onClick={() => setTeamTwoCategory(item) & setTeamTwoCategoryCheck(!teamTwoCategoryCheck)}>
@@ -67,21 +53,13 @@ function Home() {
                 </p>
               ))
             }
-
           </section>
-
         </section>
-
       </section>
-
       <section className='SecondLine'>
-
         <section className='TeamClass'>
-
           <button className='TeamClassButton' onClick={() => setTeamOneClassCheck(!teamOneClassCheck)}>Team One Class</button>
-
           <section className={teamOneClassCheck ? 'DropdownClassActive' : 'DropdownClassInactive'}>
-
             {
               (teamOneCategory === "League")
                 ?
@@ -97,17 +75,11 @@ function Home() {
                   </p>
                 )))
             }
-
           </section>
-
         </section>
-
         <section className='TeamClass'>
-
           <button className='TeamClassButton' onClick={() => setTeamTwoClassCheck(!teamTwoClassCheck)}>Team Two Class</button>
-
           <section className={teamTwoClassCheck ? 'DropdownClassActive' : 'DropdownClassInactive'}>
-
             {
               (teamTwoCategory === "League")
                 ?
@@ -123,26 +95,17 @@ function Home() {
                   </p>
                 )))
             }
-
           </section>
-
         </section>
-
       </section>
-
       <section className='ThirdLine'>
-
         <button className='RunButton' onClick={() => SelectTeam()}>Run</button>
-
         <section className='Teams'>
           <h1>{teamOne}</h1>
           <h1>{teamTwo}</h1>
         </section>
-
       </section>
-
     </div>
   )
 }
-
 export default Home;
